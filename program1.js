76ddb1bd9849f526296e408c253c1d532084cf4b
Gajendra Gour <gajendragour8747@gmail.com>
2024-09-25 12:46:09 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,31 +5,33 @@ const getTotalIsles = function (grid) {
 
   const rows = grid.length;
   const cols = grid[0].length;
-  const visited = Array(rows).fill().map(() => Array(cols).fill(false));
+  const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+  let islandCount = 0;
 
-  let count = 0;
-
-  const dfs = function (r, c) {
-    if (r < 0 || r >= rows || c < 0 || c >= cols || visited[r][c] || grid[r][c] === 'W') {
-      return;
-    }
-    visited[r][c] = true;
-    // directions
-    dfs(r - 1, c); // up
-    dfs(r + 1, c); // down
-    dfs(r, c - 1); // left
-    dfs(r, c + 1); // right
-  };
-
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === 'L' && !visited[i][j]) {
-        dfs(i, j);
-        count++;
+  function dfs(r, c) {
+      
+      if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 'W' || visited[r][c]) {
+          return;
       }
-    }
+      
+      visited[r][c] = true;
 
+      
+      dfs(r - 1, c); 
+      dfs(r + 1, c); 
+      dfs(r, c - 1); 
+      dfs(r, c + 1); 
   }
+
+  for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+          if (grid[r][c] === 'L' && !visited[r][c]) {
+              
+              islandCount++;
+              dfs(r, c); 
+          }
+      }
+
 
 };
 
