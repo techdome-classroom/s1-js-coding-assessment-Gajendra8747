956a5125bd03e0fc3a14053b120f9ad1da56d3cb
Gajendra Gour <gajendragour8747@gmail.com>
2024-09-25 12:48:47 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,6 +1,4 @@
 const getTotalIsles = function (grid) {
-
-
   if (!grid || grid.length === 0) return 0;
 
   const rows = grid.length;
@@ -31,8 +29,9 @@ const getTotalIsles = function (grid) {
               dfs(r, c); 
           }
       }
-    }
+  }
 
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
